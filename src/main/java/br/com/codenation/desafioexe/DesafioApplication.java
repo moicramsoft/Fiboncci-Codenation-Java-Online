d428b91b260fb6676bd941ad9ae7fbf8616ed652
MoicraSilva <moicramsilva@gmail.com>
2020-02-16 15:24:22 -0300

    Fiboncci Codenation Java Online V2

--- src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
+++ src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
@@ -5,22 +5,26 @@ import java.util.List;
 
 public class DesafioApplication {
 
-	public static List<Integer> fibonacci() {
-		  List<Integer> AdicionaNaLista = new ArrayList<>();
+    public static List<Integer> fibonacci() {
+        // instânciamos a ArrayList
+        List<Integer> AdicionaNaLista = new ArrayList<>();
+        // percorremos o loop  
+        for (int a = 0, b = 1; a <= 377; b += a, a = b - a) {
+            //Adionamos elemento na lista
+            AdicionaNaLista.add(a);
+        }
 
-		for(int a=0, b=1; a<=377; b+=a, a=b-a) {
-			AdicionaNaLista.add(a);
-		}
-
-		return AdicionaNaLista;
+        return AdicionaNaLista;
     }
 
-		public static Boolean isFibonacci(Integer a) {
-		return (fibonacci().contains(a));
-	}
- public static void main(String[] args) {
+    public static Boolean isFibonacci(Integer a) {
+        //Verificar se o elemento existe na ArrayList
+        return (fibonacci().contains(a));
+    }
+
+    public static void main(String[] args) {
         DesafioApplication.fibonacci();
         //DesafioApplication.isFibonacci((a));
-            
-}
+
+    }
 }
